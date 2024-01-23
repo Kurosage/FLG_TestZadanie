fcdf733bb46452bd49cb458773dd0740bb563fd2
Kurosage <asuna35981@gmail.com>
2024-01-24 00:14:53 +0300

    begin

---
+++ flg_test/src/App.vue
@@ -0,0 +1,46 @@
+
+<template>
+    <Head></Head>
+    <Footer></Footer>
+    <Main></Main>
+    <div>{{ res["data"] }}</div>
+</template>
+<script>
+import Head from "./components/Head.vue"
+import Footer from "./components/Footer.vue"
+import Main from "./components/Main.vue"
+import axios from "axios"
+import {ref} from 'vue'
+export default{
+    
+    components:{
+        Head,
+        Footer,
+        Main
+    },
+    mounted() {
+        axios.get('https://www.boredapi.com/api/activity').then(response => (this.res = response));
+    },
+    methods: {
+        
+    },
+    setup(){
+        const res = ref(0)
+        return{
+            res
+        }
+    }
+    // data() {
+    //         return {
+    //             res: null
+    //         };
+    //     },
+        
+    };
+</script>
+
+
+
+<style scoped>
+
+</style>
